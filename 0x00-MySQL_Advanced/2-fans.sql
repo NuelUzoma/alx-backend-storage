c9bb1d1b2d4f91a255fcac3a2e862441d89bf405
NuelUzoma <emcool17@gmail.com>
2023-07-14 08:57:32 +0100

    Code fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- A SQL Script that ranks origin of bands, ordered by the number
 -- of (non-unique) fans
 -- A SQL table dump will be imported
-SELECT origin FROM metal_bands ORDER BY fans ASC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
